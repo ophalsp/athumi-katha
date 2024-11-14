6977eb4655956e611efc3d1dd0fd2fbd0ba95582
Peter Ophals <peter.ophals@optis.be>
2024-11-14 11:10:54 +0100

    Split logic for all different wines

--- src/main/kotlin/be/athumi/WineShop.kt
+++ src/main/kotlin/be/athumi/WineShop.kt
@@ -8,53 +8,49 @@ class WineShop(var items: List<Wine>) {
                 if (wine.price < 0) {
                     wine.price = 0
                 }
-            } else {
-                if (wine.name != "Bourdeaux Conservato" &&
-                    wine.name != "Bourgogne Conservato" &&
-                    !wine.name.startsWith("Event")
-                ) {
-                    if (wine.price > 0) {
-                        wine.price -= 1
+            } else if (wine.name == "Bourdeaux Conservato" || wine.name == "Bourgogne Conservato") {
+                wine.expiresInYears -= 1
+                if (wine.expiresInYears < 0) {
+                    if (wine.price < 100) {
+                        wine.price += 1
+                    }
+                    if (wine.price < 100) {
+                        wine.price += 1
                     }
                 } else {
                     if (wine.price < 100) {
-                        wine.price = wine.price + 1
-
-                        if (wine.name.startsWith("Event")) {
-                            if (wine.expiresInYears < 8) {
-                                if (wine.price < 100) {
-                                    wine.price += 1
-                                }
-                            }
-
-                            if (wine.expiresInYears < 3) {
-                                if (wine.price < 100) {
-                                    wine.price += 2
-                                }
-                            }
-                        }
+                        wine.price += 1
                     }
                 }
-
+            } else if (wine.name.startsWith("Event")) {
                 wine.expiresInYears -= 1
-
-
-                if (wine.expiresInYears < 0) {
-                    if (!wine.name.contains("Conservato")) {
-                        if (!wine.name.contains("Event")) {
-                            if (wine.price > 0) {
-                                wine.price -= 1
-                            }
-                        } else {
-                            wine.price = 0
-                        }
-                    } else {
-                        if (wine.price < 100) {
-                            wine.price += 1
-                        }
+                if (wine.price < 100) {
+                    wine.price += 1
+                }
+                if (wine.expiresInYears < 7) {
+                    if (wine.price < 100) {
+                        wine.price += 1
                     }
                 }
 
+                if (wine.expiresInYears < 2) {
+                    if (wine.price < 100) {
+                        wine.price += 2
+                    }
+                }
+                if (wine.expiresInYears < 0) {
+                    wine.price = 0
+                }
+            } else {
+                if (wine.price > 0) {
+                    wine.price -= 1
+                }
+                wine.expiresInYears -= 1
+                if (wine.expiresInYears < 0) {
+                    if (wine.price > 0) {
+                        wine.price -= 1
+                    }
+                }
                 if (wine.price < 0) {
                     wine.price = 0
                 }
