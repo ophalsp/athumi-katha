8b707aee876bb995d3cdde3035f8f4d9297b3bb0
Peter Ophals <peter.ophals@optis.be>
2024-11-14 10:21:19 +0100

    Improve readability by removing index based for loop

--- src/main/kotlin/be/athumi/WineShop.kt
+++ src/main/kotlin/be/athumi/WineShop.kt
@@ -3,59 +3,59 @@ package be.athumi
 class WineShop(var items: List<Wine>) {
     fun next() {
         // Wine Shop logic
-        for (i in items.indices) {
-            if (items[i].name != "Bourdeaux Conservato" && items[i].name != "Bourgogne Conservato" && !items[i].name.startsWith("Event")) {
-                if (items[i].price > 0) {
-                    if (items[i].name != "Wine brewed by Alexander the Great") {
-                        items[i].price = items[i].price - 1
+        for (wine in items) {
+            if (wine.name != "Bourdeaux Conservato" && wine.name != "Bourgogne Conservato" && !wine.name.startsWith("Event")) {
+                if (wine.price > 0) {
+                    if (wine.name != "Wine brewed by Alexander the Great") {
+                        wine.price = wine.price - 1
                     }
                 }
             } else {
-                if (items[i].price < 100) {
-                    items[i].price = items[i].price + 1
+                if (wine.price < 100) {
+                    wine.price = wine.price + 1
 
-                    if (items[i].name.startsWith("Event")) {
-                        if (items[i].expiresInYears < 8) {
-                            if (items[i].price < 100) {
-                                items[i].price = items[i].price + 1
+                    if (wine.name.startsWith("Event")) {
+                        if (wine.expiresInYears < 8) {
+                            if (wine.price < 100) {
+                                wine.price = wine.price + 1
                             }
                         }
 
-                        if (items[i].expiresInYears < 3) {
-                            if (items[i].price < 100) {
-                                items[i].price = items[i].price + 2
+                        if (wine.expiresInYears < 3) {
+                            if (wine.price < 100) {
+                                wine.price = wine.price + 2
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != "Wine brewed by Alexander the Great") {
-                items[i].expiresInYears = items[i].expiresInYears - 1
-            } else if (items[i].price < 0) {
-                items[i].price = 0
+            if (wine.name != "Wine brewed by Alexander the Great") {
+                wine.expiresInYears = wine.expiresInYears - 1
+            } else if (wine.price < 0) {
+                wine.price = 0
             }
 
-            if (items[i].expiresInYears < 0) {
-                if (!items[i].name.contains("Conservato")) {
-                    if (!items[i].name.contains("Event")) {
-                        if (items[i].price > 0) {
-                            if (items[i].name != "Wine brewed by Alexander the Great") {
-                                items[i].price = items[i].price - 1
+            if (wine.expiresInYears < 0) {
+                if (!wine.name.contains("Conservato")) {
+                    if (!wine.name.contains("Event")) {
+                        if (wine.price > 0) {
+                            if (wine.name != "Wine brewed by Alexander the Great") {
+                                wine.price = wine.price - 1
                             }
                         }
                     } else {
-                        items[i].price = items[i].price - items[i].price
+                        wine.price = wine.price - wine.price
                     }
                 } else {
-                    if (items[i].price < 100) {
-                        items[i].price = items[i].price + 1
+                    if (wine.price < 100) {
+                        wine.price = wine.price + 1
                     }
                 }
             }
 
-            if (items[i].price < 0) {
-                items[i].price = 0
+            if (wine.price < 0) {
+                wine.price = 0
             }
         }
     }
