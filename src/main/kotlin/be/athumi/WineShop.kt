59395055bd568d78a15effa95bcfd6cd0088431c
Peter Ophals <peter.ophals@optis.be>
2024-11-14 10:31:08 +0100

    Extract multiple if statements for Wine brewed by Alexander the Great and reduce it to only one

--- src/main/kotlin/be/athumi/WineShop.kt
+++ src/main/kotlin/be/athumi/WineShop.kt
@@ -4,58 +4,60 @@ class WineShop(var items: List<Wine>) {
     fun next() {
         // Wine Shop logic
         for (wine in items) {
-            if (wine.name != "Bourdeaux Conservato" && wine.name != "Bourgogne Conservato" && !wine.name.startsWith("Event")) {
-                if (wine.price > 0) {
-                    if (wine.name != "Wine brewed by Alexander the Great") {
-                        wine.price = wine.price - 1
-                    }
+            if (wine.name == "Wine brewed by Alexander the Great") {
+                if (wine.price < 0) {
+                    wine.price = 0
                 }
             } else {
-                if (wine.price < 100) {
-                    wine.price = wine.price + 1
-
-                    if (wine.name.startsWith("Event")) {
-                        if (wine.expiresInYears < 8) {
-                            if (wine.price < 100) {
-                                wine.price = wine.price + 1
-                            }
-                        }
-
-                        if (wine.expiresInYears < 3) {
-                            if (wine.price < 100) {
-                                wine.price = wine.price + 2
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (wine.name != "Wine brewed by Alexander the Great") {
-                wine.expiresInYears = wine.expiresInYears - 1
-            } else if (wine.price < 0) {
-                wine.price = 0
-            }
-
-            if (wine.expiresInYears < 0) {
-                if (!wine.name.contains("Conservato")) {
-                    if (!wine.name.contains("Event")) {
-                        if (wine.price > 0) {
-                            if (wine.name != "Wine brewed by Alexander the Great") {
-                                wine.price = wine.price - 1
-                            }
-                        }
-                    } else {
-                        wine.price = wine.price - wine.price
+                if (wine.name != "Bourdeaux Conservato" &&
+                    wine.name != "Bourgogne Conservato" &&
+                    !wine.name.startsWith("Event")
+                ) {
+                    if (wine.price > 0) {
+                        wine.price -= 1
                     }
                 } else {
                     if (wine.price < 100) {
                         wine.price = wine.price + 1
+
+                        if (wine.name.startsWith("Event")) {
+                            if (wine.expiresInYears < 8) {
+                                if (wine.price < 100) {
+                                    wine.price += 1
+                                }
+                            }
+
+                            if (wine.expiresInYears < 3) {
+                                if (wine.price < 100) {
+                                    wine.price += 2
+                                }
+                            }
+                        }
                     }
                 }
-            }
 
-            if (wine.price < 0) {
-                wine.price = 0
+                wine.expiresInYears -= 1
+
+
+                if (wine.expiresInYears < 0) {
+                    if (!wine.name.contains("Conservato")) {
+                        if (!wine.name.contains("Event")) {
+                            if (wine.price > 0) {
+                                wine.price -= 1
+                            }
+                        } else {
+                            wine.price = 0
+                        }
+                    } else {
+                        if (wine.price < 100) {
+                            wine.price += 1
+                        }
+                    }
+                }
+
+                if (wine.price < 0) {
+                    wine.price = 0
+                }
             }
         }
     }
