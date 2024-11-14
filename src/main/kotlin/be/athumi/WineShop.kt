d75eb7a7ecd7c9d43f4a031aeaf6fe7b3389a0cd
Peter Ophals <peter.ophals@optis.be>
2024-11-14 12:36:30 +0100

    Split logic to improve readability

--- src/main/kotlin/be/athumi/WineShop.kt
+++ src/main/kotlin/be/athumi/WineShop.kt
@@ -5,56 +5,56 @@ class WineShop(var items: List<Wine>) {
         // Wine Shop logic
         for (wine in items) {
             if (wine.name == "Wine brewed by Alexander the Great") {
-                if (wine.price < 0) {
-                    wine.price = 0
-                }
-            } else if (wine.name == "Bourdeaux Conservato" || wine.name == "Bourgogne Conservato") {
-                wine.expiresInYears -= 1
-                if (wine.expiresInYears < 0) {
-                    if (wine.price < 100) {
-                        wine.price += 1
-                    }
-                    if (wine.price < 100) {
-                        wine.price += 1
-                    }
-                } else {
-                    if (wine.price < 100) {
-                        wine.price += 1
-                    }
-                }
-            } else if (wine.name.startsWith("Event")) {
-                wine.expiresInYears -= 1
-                if (wine.price < 100) {
-                    wine.price += 1
-                }
-                if (wine.expiresInYears < 7) {
-                    if (wine.price < 100) {
-                        wine.price += 1
-                    }
-                }
-
-                if (wine.expiresInYears < 2) {
-                    if (wine.price < 100) {
-                        wine.price += 2
-                    }
-                }
-                if (wine.expiresInYears < 0) {
-                    wine.price = 0
-                }
+                //do nothing
             } else {
-                if (wine.price > 0) {
-                    wine.price -= 1
-                }
                 wine.expiresInYears -= 1
-                if (wine.expiresInYears < 0) {
-                    if (wine.price > 0) {
-                        wine.price -= 1
-                    }
-                }
-                if (wine.price < 0) {
-                    wine.price = 0
+                if (wine.name.contains("Conservato")) {
+                    incrementPriceForAgingWine(wine)
+                } else if (wine.name.startsWith("Event")) {
+                    incrementPriceForEventWine(wine)
+                } else {
+                    decrementPriceForStandardWine(wine)
                 }
             }
+            if (wine.price < 0) {
+                wine.price = 0
+            }
         }
+    }
+
+    private fun decrementPriceForStandardWine(wine: Wine) {
+        wine.price -= 1
+        if (wine.expiresInYears < 0) {
+            wine.price -= 1
+        }
+    }
+
+    private fun incrementPriceForAgingWine(wine: Wine) {
+        if (wine.expiresInYears < 0) {
+            wine.price = incrementPriceForAgingWine(wine, 2)
+        } else {
+            wine.price = incrementPriceForAgingWine(wine, 1)
+        }
+    }
+
+    private fun incrementPriceForEventWine(wine: Wine) {
+        wine.price = incrementPriceForAgingWine(wine, 1)
+        if (wine.expiresInYears < 7) {
+            wine.price = incrementPriceForAgingWine(wine, 1)
+        }
+        if (wine.expiresInYears < 2) {
+            wine.price = incrementPriceForAgingWine(wine, 2)
+        }
+        if (wine.expiresInYears < 0) {
+            wine.price = 0
+        }
+    }
+
+    private fun incrementPriceForAgingWine(wine: Wine, increment: Int): Int {
+        //TODO: check what to do if the price is already bigger than 100 to start with
+        if (wine.price > 100) {
+            return wine.price
+        }
+        return (wine.price + increment).coerceAtMost(100)
     }
 }
