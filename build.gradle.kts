4d5b24322ca5a871c69bf7fec2f95270fc3a739a
Peter Ophals <peter.ophals@optis.be>
2024-11-14 09:45:49 +0100

    Add approvaltests library, add test for covering all legacy code, add .editorconfig to make sure IntelliJ does not trim trailing whitespaces

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,12 @@ tasks.test {
 
 dependencies {
     val junitVersion = "3.26.3"
+    val approvalTestsVersion = "24.9.0"
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testImplementation("org.assertj:assertj-core:$junitVersion")
+    testImplementation("com.approvaltests:approvaltests:$approvalTestsVersion")
 
     testCompileOnly("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testCompileOnly("org.junit.jupiter:junit-jupiter-params:$junitVersion")
--- src/test/kotlin/be/athumi/WineTest.kt
+++ src/test/kotlin/be/athumi/WineTest.kt
@@ -1,5 +1,6 @@
 package be.athumi
 
+import org.approvaltests.combinations.CombinationApprovals.verifyAllCombinations
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -12,11 +13,30 @@ class WineTest {
     @Test
     fun `a shop without wine is no shop, is it`() {
         val shop = WineShop(listOf(Wine("name", 0, 0)))
-
         assertThat(shop).isNotNull
-
         shop.next()
-
         assertThat(shop).isNotNull
     }
+
+    @Test
+    fun testLegacyCode() {
+        val wines = arrayOf(
+            "Standard Wine", "Bourdeaux Conservato", "Bourgogne Conservato", "Another Standard Wine",
+            "Wine brewed by Alexander the Great", "Event Wine", "Eco Brilliant Wine"
+        )
+        val winePrices = arrayOf(-1, 0, 7, 20, 99, 101)
+        val wineExpiresInYears = arrayOf(-1, 0, 1, 2, 3, 5, 10, 15)
+        verifyAllCombinations(
+            { wineName, winePrice, wineExpiresIn ->
+                val wine = Wine(wineName, winePrice, wineExpiresIn)
+                val shop = WineShop(listOf(wine))
+                shop.next()
+                wine
+            },
+            wines,
+            winePrices,
+            wineExpiresInYears
+        )
+    }
+
 }
